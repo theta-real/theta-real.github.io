3bfe7987657fbb75dded67c30f0c80504135bdce
theta-real <xhfan87@gmail.com>
2025-09-14 16:07:36 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
